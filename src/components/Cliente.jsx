c702a97a1e60b889173b93e3c1fa9d5b4ab6c859
Jemery Inoa <jeremyinoa67@gmail.com>
2023-04-02 12:25:32 -0400

    form with edit/delete buttons added

--- src/components/Cliente.jsx
+++ src/components/Cliente.jsx
@@ -39,6 +39,18 @@ function Cliente({cliente}){
                 {descVestido}    
             </span>
         </p>
+        <div className="flex justify-between mt-10">
+            <button type="button" 
+            className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold rounded-lg"
+            >
+                Editar
+            </button>
+            <button type="button"
+            className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold rounded-lg"
+            >
+                Eliminar
+            </button>
+        </div>
     </div>
   )
 }
