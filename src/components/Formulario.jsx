b43a29a8b7a8cab9bbe67e5716c1ccd57433e685
Jemery Inoa <jeremyinoa67@gmail.com>
2023-04-02 20:06:38 -0400

    changed from 'pacientes' to  'clientes'

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -154,7 +154,7 @@ function Formulario( {clientes, setClientes, cliente}){
                 <input
                     type="submit"
                     className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-all rounded-md"
-                    value={ cliente.id ? "Editar Paciente": "Agregar Paciente"}
+                    value={ cliente.id ? "Editar Cliente": "Agregar Cliente"}
                 />
             </form>
         </div>
